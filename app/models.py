abd30481fe1bb045fa19e89b2735b8da81191dde
kelley <0014121@student.isf.edu.hk>
2024-10-14 11:56:05 +0800

    #today I was able to finish testing my MVP models and almost done with testing all my models, #next I will finish up the testing models and start on views.

--- app/models.py
+++ app/models.py
@@ -1,3 +1,12 @@
 # models.py
 
 from banjo.models import Model, StringField, IntegerField, FloatField, BooleanField
+
+class Quotes(Model):
+    quote = StringField()
+    hint = StringField()
+    likes = IntegerField()
+    difficulty = IntegerField()
+    correct_percentage = FloatField()
+
+
