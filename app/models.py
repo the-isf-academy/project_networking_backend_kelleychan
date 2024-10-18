d219e387e0dd2abf6615bc1a6b5553b3a46fbe0c
kelley <0014121@student.isf.edu.hk>
2024-10-18 09:10:00 +0800

    #today I finished all my MVPS, #next  I will start working on the other routes and methods such as quote of the day and the quote popularity leaderboard, etc.

--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
 # models.py
 
 from banjo.models import Model, StringField, IntegerField, FloatField, BooleanField
+from fuzzywuzzy import fuzz
 
 class Quotes(Model):
     quote = StringField()
@@ -12,6 +13,9 @@ class Quotes(Model):
     guesses = IntegerField()
     correct = IntegerField()
 
+    MIN_FUZZ_RATIO = 80
+
+
 
     def json_response(self):
             
@@ -20,6 +24,8 @@ class Quotes(Model):
                 'quote': self.quote,
                 'likes': self.likes,
                 'difficulty': self.difficulty,
+                'guesses' : self.guesses,
+                'answer' : self.answer
             }
     
     def json_response_answerless(self):
@@ -27,7 +33,10 @@ class Quotes(Model):
             "id": self.id,
             "quote": self.quote,
             "correct": self.correct,
-            "guesses": self.guesses
+            'difficulty': self.difficulty,
+            'hint' : self.hint,
+            'guesses': self.guesses
+
         }
     
     def json_response_incorrect_guess(self):
@@ -35,6 +44,7 @@ class Quotes(Model):
             {'id': self.id,
             'question': self.quote,
             'guesses': self.guesses,
+            'difficulty': self.difficulty,
             'correct': False}
         )
     
@@ -43,5 +53,22 @@ class Quotes(Model):
             {'id': self.id,
             'question': self.quote,
             'guesses': self.guesses,
+            'difficulty': self.difficulty,
             'correct': True}
         )
+    def check_guess(self, guess):
+
+        self.guesses += 1
+        similarity = fuzz.ratio(guess.lower(), self.answer.lower())
+            
+        if similarity >= self.MIN_FUZZ_RATIO:
+            self.correct+= 1
+            self.save()
+            return True
+        else:
+            self.save()
+            return False
+            
+    def add_hint(self, new_hint):
+            self.hint = new_hint
+            self.save()
--- app/views.py
+++ app/views.py
@@ -3,6 +3,7 @@
 from banjo.urls import route_get, route_post
 from settings import BASE_URL
 from .models import Quotes
+from fuzzywuzzy import fuzz
 
 @route_post(BASE_URL + 'newquote', args={'quote':str, 'difficulty': str, 'answer': str})
 def new_quote(args):
@@ -22,7 +23,49 @@ def all_quotes(args):
     quote_list = []
 
     for quote in Quotes.objects.all():
+        quote_list.append(quote.json_response_answerless())
+
+    return {'quotes':quote_list}
+
+
+@route_post(BASE_URL + 'guess', args={'guess': str, 'id': int})
+def guess_quote(args):
+    if Quotes.objects.filter(id=args['id']).exists():
+        one_quote = Quotes.objects.get(id=args['id'])
+
+        if one_quote.check_guess(args['guess']) == True:
+            return {'Quote': one_quote.json_response_correct_guess()}
+        
+        else:
+            return {'Quote': one_quote.json_response_incorrect_guess()}
+        
+    else:
+        return{'error': 'no such quote'}
+    
+@route_get(BASE_URL + 'allanswers')
+def all_quotes(args):
+    quote_list = []
+
+    for quote in Quotes.objects.all():
         quote_list.append(quote.json_response())
 
     return {'quotes':quote_list}
 
+@route_get(BASE_URL + 'difficultychange', args={'difficulty':str})
+def searchword(args):
+   quote_list = []
+
+   for searchword in Quotes.objects.filter(difficulty__contains= args['difficulty']):
+       quote_list.append(searchword.json_response_answerless())
+    
+       return {'quotes':quote_list}
+
+   else:
+       return {'error': 'no riddle exists'}
+
+@route_post(BASE_URL + 'addhint', args={'id':int, 'new_hint':str})
+def add_hints(args):
+    if Quotes.objects.filter(id=args['id']).exists():
+        add_hints = Quotes.objects.get(id=args['id'])
+        add_hints.add_hint(args['new_hint'])
+        return {'Fortune': add_hints.json_response_answerless()}
